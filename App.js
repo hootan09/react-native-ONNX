2c77226c42820c29bf05a16970ca8e63a8dd488d
niki <hootan09@gmail.com>
2023-11-26 20:50:44 +0330

    reading image pixel

--- App.js
+++ App.js
@@ -4,17 +4,28 @@ import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 import * as ort from 'onnxruntime-react-native';
 import { Asset } from 'expo-asset';
 import { useState } from 'react';
+import * as FileSystem from 'expo-file-system';
+import { Buffer } from 'buffer';
 
 export default function App() {
   
   const [model, setModel] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null);
   const [modelStatus, setModelStatus] = useState('');
   const [runResult, setRunResult] = useState('');
 
   const loadModel = async()=>{
     try {
-      const assets = await Asset.loadAsync(require('./assets/models/mnist.ort'));
-      const modelUri = assets[0].localUri;
+      const assets = await Asset.loadAsync([require('./assets/models/mnist.ort'),require('./assets/3.jpg')]);
+
+      const modelUri = assets[0]?.localUri;
+
+      const imageUri = assets[1]?.localUri;
+      if(imageUri){
+        const base64 = await FileSystem.readAsStringAsync(imageUri, { encoding: 'base64' });
+        setImageBase64(base64)
+      }
+
       if (!modelUri) {
         // Alert.alert('failed to get model URI', `${assets[0]}`);
         console.log('Failed to get model URI', `${assets[0]}`);
@@ -37,22 +48,35 @@ export default function App() {
       throw e;
     }
   }
-  
+
   const runModel = async()=> {
     try {
-      const inputData = new Float32Array(28 * 28);
-      const feeds = {};
-      feeds[model.inputNames[0]] = new ort.Tensor(inputData, [1,1, 28, 28]);
+      // const inputData = new Float32Array(28 * 28);
+      // const feeds = {};
+      // feeds[model.inputNames[0]] = new ort.Tensor(inputData, [1,1, 28, 28]);
+      // const fetches = await model.run(feeds);
+      // const output = fetches[model.outputNames[0]];
+      
+      const bytes = Buffer.from(imageBase64, 'base64');
+      const inputData = new Float32Array(784);
+      for (let i = 0, len = bytes.length; i < len; i += 4) {
+        inputData[i / 4] = bytes[i + 3] / 255;
+      }
+      const inputTensor = new ort.Tensor('float32', inputData, [1, 1, 28, 28]);
+      // prepare feeds. use model input names as keys.
+      const feeds = { Input3: inputTensor };
       const fetches = await model.run(feeds);
       const output = fetches[model.outputNames[0]];
+      
       if (!output) {
         // Alert.alert('failed to get output', `${model.outputNames[0]}`);
         console.log('Failed to get output', `${model.outputNames[0]}`);
         setRunResult(`Failed to get output ${model.outputNames[0]}`);
       } else {
         // Alert.alert(
-        //   'model inference successfully',
-        //   `output shape: ${output.dims}, output data: ${output.data}`);
+          //   'model inference successfully',
+          //   `output shape: ${output.dims}, output data: ${output.data}`);
+        console.log(getPredictedClass(output?.data));
         console.log('Model inference successfully',`output shape: ${output.dims}, output data: ${output.data}`);
         setRunResult(`Model inference successfully output shape: ${output.dims}, output data: ${output.data}`)
       }
@@ -64,7 +88,12 @@ export default function App() {
     }
   }
 
-
+  const getPredictedClass = (output) =>  {
+    if (output.reduce((a, b) => a + b, 0) === 0) { 
+      return -1;
+    }
+    return output.reduce((argmax, n, i) => (n > output[argmax] ? i : argmax), 0);
+  }
 
   return (
     <View style={styles.container}>
